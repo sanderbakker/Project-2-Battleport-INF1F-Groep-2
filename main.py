4868824eda56f64604624c721a84cb6d76cb1c66
Lennart Voogd <lennartvoogd@live.nl>
2017-01-17 13:48:31 +0100

    Basic folder structure for the application

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+
+Play the game by running this file.
+
+"""
+
+# Import the required modules 
+import pygame, sys, pymysql
+
+# Import all files stated in the __all__ variable in modules/__init__.py
+from modules import *
