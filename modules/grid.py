d118ff98e4013616e6873025c104e4d4a8b3e2df
DESKTOP-TN6G1OD\Sander <sanderbakker1999@gmail.com>
2017-01-17 16:11:49 +0100

    Added ship class for testing, made a class instead of function for the grid

--- modules/grid.py
+++ modules/grid.py
@@ -9,35 +9,64 @@ height = 600
 
 display = ((width_screen, height))
 screen = pygame.display.set_mode(display)
+class Grid: 
+    def __init__ (self):
+        self.x = 0 
+        self.y = 0
+        #self.width = 0
+    #def Place(self, x, y):
+        
+ 
+    def grid(self, width, x_blocks, y_blocks):
+        width = width/x_blocks 
+        #while x < width:
+        #    pygame.draw.line(screen, WHITE, ((x + 20), y), (x + 20, height), (1))
+        #    x = x + 20 
+        #while y < height:
+        #    x = 0
+        #    pygame.draw.line(screen, WHITE, (x, y + 20), (width, y + 20), (1))
+        #    y = y + 20
 
-def grid(width, x_blocks, y_blocks):
-    y = 0 
-    x = 0 
-    width = width/x_blocks 
-
-
-    for l in range(int(width)):
-        if x_blocks > 0:
-            if x <= opacity_grid: 
-                pygame.draw.line(screen, WHITE, ((x + int(width)), y), (x + int(width), opacity_grid), (1))
-                x = x + int(width)
-                x_blocks = x_blocks - 1
-            else:
-                pygame.draw.line(screen, WHITE, ((x + int(width)), y), (x + int(width), opacity_grid), (1))
-                x = x + int(width)
-    for m in range(int(width)):
-        x = 0  
-        if y_blocks > 0:
-            if y <= opacity_grid:
-                pygame.draw.line(screen, WHITE, (x, (y + int(width))), (opacity_grid, y + int(width)), (1))
-                y = y + int(width)
-                y_blocks = y_blocks - 1 
-            else:
-                pygame.draw.line(screen, WHITE, (x, (y + int(width))), (opacity_grid, y + int(width)), (1))
-                y = y + int(width)
+        for l in range(int(width)):
+            if x_blocks > 0:
+                if self.x <= opacity_grid: 
+                    pygame.draw.line(screen, WHITE, ((self.x + int(width)), self.y), (self.x + int(width), opacity_grid), (1))
+                    self.x = self.x + int(width)
+                    x_blocks = x_blocks - 1
+                else:
+                    pygame.draw.line(screen, WHITE, ((self.x + int(width)), self.y), (self.x + int(width), opacity_grid), (1))
+                    self.x = self.x + int(width)
+        for m in range(int(width)):
+            self.x = 0  
+            if y_blocks > 0:
+                if self.y <= opacity_grid:
+                    pygame.draw.line(screen, WHITE, (self.x, (self.y + int(width))), (opacity_grid, self.y + int(width)), (1))
+                    self.y = self.y + int(width)
+                    y_blocks = y_blocks - 1 
+                else:
+                    pygame.draw.line(screen, WHITE, (self.x, (self.y + int(width))), (opacity_grid, self.y + int(width)), (1))
+                    self.y = self.y + int(width)
     
 opacity_grid = 400
-grid(int(opacity_grid), 20, 20)
+Grid().grid(int(opacity_grid), 20, 20)
+
+
+
+class Ship:
+    def __init__(self, x, y, r): 
+        self.x = x
+        self.y = y
+        self.r = r
+    def draw(self, screen):
+        pygame.draw.circle(screen, (0, 255, 0),
+                           (int(self.x), int(self.y)), int(self.r))
+
+
+ship = Ship(10, 10, 5)
+ship.draw(screen)
 pygame.display.flip()
+
+
+
 
 
