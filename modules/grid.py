1ca7a3a36fb7a39daca859b110d785e457bb289e
DESKTOP-TN6G1OD\Sander <sanderbakker1999@gmail.com>
2017-01-18 13:07:55 +0100

    Added option to move the grid, blocks moves equal with the grid

--- modules/grid.py
+++ modules/grid.py
@@ -12,12 +12,40 @@ display = ((width_screen, height))
 screen = pygame.display.set_mode(display)
 #make a display
 class Grid: 
-    def __init__ (self, x_blocks, y_blocks, opacity_grid):
+    def __init__ (self, x_blocks, y_blocks, opacity_grid, move_grid):
+        self.number_of_blocks = x_blocks
         self.x = 0 
         self.y = 0
         self.x_blocks = x_blocks    
         self.y_blocks = y_blocks 
         self.opacity_grid = opacity_grid
+        self.move_grid = move_grid
+        #drawing the horizontal part of the grid 
+        opacity_grid = self.opacity_grid + self.move_grid
+        width = self.opacity_grid/self.x_blocks 
+        pygame.draw.line(screen, WHITE, (self.move_grid, self.move_grid), (self.move_grid, opacity_grid), (1))
+        pygame.draw.line(screen, WHITE, (self.move_grid, self.move_grid), (opacity_grid, self.move_grid), (1))
+        for l in range(int(width)):
+            if self.x_blocks > 0:
+                if self.x <= opacity_grid + self.move_grid: 
+                    pygame.draw.line(screen, WHITE, ((self.x + int(width) + self.move_grid), self.y + self.move_grid), (self.x + int(width) + self.move_grid, self.opacity_grid + self.move_grid), (1))
+                    self.x = self.x + int(width)
+                    self.x_blocks = self.x_blocks - 1
+                #else:
+                #    pygame.draw.line(screen, WHITE, ((self.x + int(width)), self.y + self.move_grid), (self.x + int(width), self.opacity_grid + self.move_grid), (1))
+                #    self.x = self.x + int(width)
+                 
+#drawing the vertical part of the grid                 
+        for m in range(int(width)):
+            self.x = 0  
+            if self.y_blocks > 0:
+                if self.y <= opacity_grid + self.move_grid:
+                    pygame.draw.line(screen, WHITE, (self.x + self.move_grid, (self.y + int(width + self.move_grid))), (self.opacity_grid + self.move_grid, self.y + int(width) + self.move_grid), (1))
+                    self.y = self.y + int(width)
+                    self.y_blocks = self.y_blocks - 1 
+                #else:
+                #    pygame.draw.line(screen, WHITE, (self.x + self.move_grid, (self.y + int(width))), (self.opacity_grid + self.move_grid, self.y + int(width)), (1))
+                #    self.y = self.y + int(width)
 
     def Place_Circle(self, x_ship, y_ship, r_ship):
 #first part of placing a "object" on the screen for the coordinates (1, 1)
@@ -55,41 +83,19 @@ class Grid:
         
         
  
-    def grid(self, width):
-#drawing the horizontal part of the grid 
-        width = width/self.x_blocks 
-        for l in range(int(width)):
-            if self.x_blocks > 0:
-                if self.x <= opacity_grid: 
-                    pygame.draw.line(screen, WHITE, ((self.x + int(width)), self.y), (self.x + int(width), self.opacity_grid), (1))
-                    self.x = self.x + int(width)
-                    self.x_blocks = self.x_blocks - 1
-                else:
-                    pygame.draw.line(screen, WHITE, ((self.x + int(width)), self.y), (self.x + int(width), self.opacity_grid), (1))
-                    self.x = self.x + int(width)
-                    
-#drawing the vertical part of the grid                 
-        for m in range(int(width)):
-            self.x = 0  
-            if self.y_blocks > 0:
-                if self.y <= opacity_grid:
-                    pygame.draw.line(screen, WHITE, (self.x, (self.y + int(width))), (self.opacity_grid, self.y + int(width)), (1))
-                    self.y = self.y + int(width)
-                    self.y_blocks = self.y_blocks - 1 
-                else:
-                    pygame.draw.line(screen, WHITE, (self.x, (self.y + int(width))), (self.opacity_grid, self.y + int(width)), (1))
-                    self.y = self.y + int(width)
+    #def grid(self):
 
-    def Place_Square(self, rect_x, rect_y, number_blocks):
-        middle_box = ((self.opacity_grid/number_blocks) / 2)    
-        rect_x = ((rect_x * (middle_box)) + middle_box * (rect_x - 2))
-        rect_y = ((rect_y * (middle_box)) + middle_box * (rect_y - 2))
-        pygame.draw.rect(screen, (100,200,250), pygame.Rect((int(rect_x), int(rect_y)), (number_blocks, number_blocks)))
+         
+    def Place_Square(self, rect_x, rect_y):
+        print(self.move_grid)
+        middle_box = ((self.opacity_grid/self.number_of_blocks) / 2)  
+        rect_x = ((rect_x * (middle_box)) + middle_box * (rect_x - 2)) + self.move_grid
+        rect_y = ((rect_y * (middle_box)) + middle_box * (rect_y - 2)) + self.move_grid
+        pygame.draw.rect(screen, (100,200,250), pygame.Rect((int(rect_x), int(rect_y)), (self.number_of_blocks, self.number_of_blocks)))
                 
-    
+   
 opacity_grid = 400
-Grid(20, 20, 400).grid(int(opacity_grid))
-#Grid(20, 20, opacity_grid).Place(2, 2, 5)
+
 
 
 
@@ -104,8 +110,9 @@ Grid(20, 20, 400).grid(int(opacity_grid))
     
 
     
-                                               
-Grid(20, 20, opacity_grid).Place_Square(7, 15, 20)
+                                             
+test = Grid(20, 20, opacity_grid, 100)
+test.Place_Square(20, 20)
 
 #filps the screen
 pygame.display.flip()
