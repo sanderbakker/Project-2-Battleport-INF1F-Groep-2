34dba38215d5f532698f5bb6150f4f7a686b247a
Lennart Voogd <lennartvoogd@live.nl>
2017-01-18 19:52:27 +0100

    Added ‘Turn’ class

--- modules/player.py
+++ modules/player.py
@@ -2,7 +2,6 @@ class Player:
 	def __init__(self, name):
 		self.Name = name
 		self.Score = 0
-		self.normalCards = []
 
 	# retrieves the players username.
 	def get_name(self):
@@ -32,12 +31,41 @@ class Player:
 	def subtract_score(self, number): 
 		return self.__edit_score(number, lambda x, y: x - y)
 
+
+class Turn:
+	def __init__(self, player):
+		self.normalCards = []
+		self.specialCards = []
+		self.player = player		
+
+	def get_player():
+		return self.player
+
 	# retrieve the normal cards of the player
 	def get_normal_cards():
 		return self.normalCards 
 
+	# add a normal card to the player deck
 	def add_normal_card(card):
 		self.normalCards = self.normalCards.append(card)
 		return self.normalCards
+
+	# retrieve the special cards of the player deck 
+	def get_special_cards():
+		return self.specialCards
+
+	# add a special card to the player deck
+	def add_special_cards():
+		self.specialCards = self.specialCards.append(card)
+		return self.specialCards
+
+	# get turn
+	def get_turn(self):
+		return self.Turn
+
+	# set turn
+	# Turn = bool
+	def set_turn(player):
+		self.Turn = Turn		
 
 
