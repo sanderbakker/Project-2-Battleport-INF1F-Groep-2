553fd6cb5107c254d046c83189b30e692ddcab77
Lennart Voogd <lennartvoogd@live.nl>
2017-01-19 14:50:04 +0100

    Initial commit for the card module

---
+++ modules/cards.py
@@ -0,0 +1,15 @@
+class card:
+	def __init__():
+		self.belongs_to = False
+		self.stack 		= ''
+
+class normal_card:
+	def __init__(card):
+		# set the stack to normal
+		card.set_stack('normal')
+
+class special_card:
+	def __init__(card):
+		# set the stack to special
+		card.set_stack('special')
+
