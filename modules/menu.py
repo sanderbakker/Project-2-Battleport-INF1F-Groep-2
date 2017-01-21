d05e05749355d59faa66b1eef1f27741eefadc75
DESKTOP-TN6G1OD\Sander <sanderbakker1999@gmail.com>
2017-01-21 10:09:47 +0100

    Added option to scale the buttons on the screen (everywhere the same distance between them)

--- modules/menu.py
+++ modules/menu.py
@@ -1,7 +1,7 @@
 import pygame 
 WHITE = (255 ,255 ,255)
 pygame.init()
-width_screen = 800
+width_screen = 400
 height = 575
 # Set height and width of the screen
 display = ((width_screen, height))
@@ -18,7 +18,7 @@ class Menu:
 
     def add_logo(self):
         img = pygame.image.load("logo.png")
-        screen.blit(img, ((self.distance_border + (width_screen - 380) / 2), (self.distance_border + (width_screen - (width_screen - 100)) / 2)))
+        screen.blit(img, ((self.distance_border + (width_screen/ 2), (self.distance_border + (width_screen - (width_screen - 100)) / 2))))
 
 def process_events():
     for event in pygame.event.get():
@@ -27,16 +27,25 @@ def process_events():
             return True
 
     return False
+distance_border = 20
+def draw_button(width_blocks, number_blocks):
+    space_left_with_blocks = width_screen - (width_blocks * number_blocks)
+    space_left = space_left_with_blocks - (distance_border * 2)
+    distance_between = space_left/(number_blocks+1)
+    space_between = width_blocks + distance_between
+    start_point = distance_between + distance_border
+    end_point = width_screen - int(start_point + width_blocks)
+    x = start_point
 
-def draw_button():
-    pygame.draw.rect(screen, (48, 148, 51), pygame.Rect((103, 479), (100, 35)))
-    pygame.draw.rect(screen, (48, 148, 51), pygame.Rect((356, 479), (100, 35)))
-    pygame.draw.rect(screen, (48, 148, 51), pygame.Rect((610, 479), (100, 35)))
+    while x <= end_point:
+        pygame.draw.rect(screen, (48, 148, 51), pygame.Rect((x, 479), (width_blocks, 35)))
+        x = x + space_between
+
 def program():
     while not process_events():
         Menu(20).draw_frame()
-        Menu(20).add_logo()
-        draw_button()
+        #Menu(20).add_logo()
+        draw_button(50, 3)
         pygame.display.flip()
 
 
