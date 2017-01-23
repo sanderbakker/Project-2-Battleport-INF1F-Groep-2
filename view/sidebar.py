9c8683adc13fa45f39224f53848e2f45443bf178
Lennart Voogd <lennartvoogd@live.nl>
2017-01-23 14:37:17 +0100

    Added toolbar, Menu buttons can be displayed here

--- view/sidebar.py
+++ view/sidebar.py
@@ -3,14 +3,22 @@ import pygame
 pygame.init()
 
 class Show:
-	def __init__(self, Game, Player):
+	def __init__(self, Game, Player, Menu):
 		self.display_width = Game.get_display_width()
 		self.display_height = Game.get_display_height()
 
-		self.Game = Game
+		self.Game 	= Game
 		self.Player = Player
+		self.Menu 	= Menu
+
+		""" sidebar """
 		self.width = 250 #pixels
 		self.start = Game.get_display_width() - self.width
+		self.sidebar_color = (48,148,51)
+
+		""" toolbar """
+		self.toolbar_height = 60
+		self.toolbar_color  = (42,129,44)
 
 		self.card_width = 100
 		self.card_height = 150
@@ -18,11 +26,14 @@ class Show:
 
 
 		self.draw_sidebar()
+		self.draw_toolbar()
 		#self.draw_normal_deck()
 
 	def draw_sidebar(self):
-		pygame.draw.rect(self.Game.get_screen(), (48,148,51), [self.start, 0, self.width, self.display_height])
+		pygame.draw.rect(self.Game.get_screen(), (self.sidebar_color), [self.start, 0, self.width, self.display_height])
 
+	def draw_toolbar(self):
+		pygame.draw.rect(self.Game.get_screen(), (self.toolbar_color), [self.start, 0, self.width, self.toolbar_height])
 
 	def draw_normal_deck(self):
 		self.Game.draw_text('Normal cards', ( (self.start_width_card - 15), 350))
