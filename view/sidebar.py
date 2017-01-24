12f7bd3e08eb30d03a54f8483ffc52443f4a6e95
Sander Bakker <sanderbakker1999@gmail.com>
2017-01-24 12:31:44 +0100

    Fixed issues with "help" screen

--- view/sidebar.py
+++ view/sidebar.py
@@ -67,7 +67,7 @@ class Show:
 		mouse = pygame.mouse.get_pos()
 		if mouse[1] >= 15 and mouse[1] <= 45:
 			if mouse[0] >= 700 and mouse[0] <= 775:
-				image = pygame.image.load("assets\game2.png")
+				image = pygame.image.load("assets/game2.png")
 				self.Game.get_screen().blit(image, (0, 0))
 
 	def draw_normal_deck(self):
