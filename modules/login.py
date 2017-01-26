fedb1a704fbf90d3f63e4e24deea6e22d2d574f6
Sander Bakker <sanderbakker1999@gmail.com>
2017-01-26 11:19:13 +0100

    Not possible to enter chars outside the box & cleaned up some code

--- modules/login.py
+++ modules/login.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 pygame.init()
 
 class PlayerName():
@@ -8,77 +9,54 @@ class PlayerName():
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.name1 = ""
         self.Player1 = ""
-        self.name2 = ""
-        self.Player2 = ""
-
-        self.name(400, 50, 0)
-
-    def name(self, x, y, count):
-
-        #password = ""
-        font = pygame.font.SysFont("arial", 30)
-
+        self.distance_border = 20
+        self.draw_frame()
+        self.name(400, 50)
+    def draw_frame(self):
+        pygame.draw.rect(self.screen, (204, 0, 0),
+                         pygame.Rect((self.distance_border, self.distance_border),
+                                     ((self.width - self.distance_border * 2),
+                                      (self.height - self.distance_border * 2))))
+    def name(self, x, y):
+        font = pygame.font.SysFont("Arial", 30)
         while not False:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if pygame.key.get_pressed():
                         if event.key == pygame.K_BACKSPACE:
-                            if count == 0:
-                                self.name1 = self.name1[:-1]
-                            else:
-                                self.name2 = self.name2[:-1]
+                            self.name1 = self.name1[:-1]
                         elif event.key == pygame.K_ESCAPE:
-                            if count == 0:
-                                self.name1 = ""
-                            else:
-                                self.name2 = ""
+                            self.name1 = ""
                         elif event.key == pygame.K_SPACE:
-                            if count == 0:
-                                self.name1 = self.name1
-                            else:
-                                self.name2 = self.name2
+                            self.name1 = self.name1
                         elif event.key == pygame.K_RETURN:
                             self.Player1 = self.name1
-                            self.Player2 = self.name2
-
                             return True
-
-
-                        #elif event.key == pygame.K_RETURN:
-                        #    if event.key == pygame.K_BACKSPACE:
-                        #        password = password[:-1]
-                        #    elif event.key == pygame.K_SPACE:
-                        #        password = password
-                        #    else:
-                        #        password = password + event.unicode
                         else:
-                            if count == 0:
-                                self.name1 = self.name1 + event.unicode
-                            else:
-                                self.name2 = self.name2 + event.unicode
+
+                            self.name1 = self.name1 + event.unicode
 
                 elif event.type == pygame.QUIT:
-                    return True
-
-
-            self.screen.fill((212, 212, 212))
-            text = font.render(self.name1, 1, (255, 255, 255))
-            text2 = font.render(self.name2, 1, (255,255,255))
+                    sys.exit()
 
 
 
+            #self.screen.fill((212, 212, 212))
+            username_1 = font.render(self.name1, 1, (255, 255, 255))
+            username_1_position = username_1.get_rect()
 
-            ask = font.render("Enter your username", 1 , (255,255,255))
+            ask = font.render("Player 1, enter your username", 1 , (255,255,255))
             ask_position = ask.get_rect()
             pygame.draw.rect(self.screen, (100, 100, 100), pygame.Rect(self.width/2 - (x/2), self.height/2 - (y/2), 400, 50))
-            self.screen.blit(text, (self.width /2 -  (ask_position[2]/2) - 80, self.height/2 - (ask_position[3]/2)))
-            self.screen.blit(text2,
-                             (self.width / 2 - (ask_position[2] / 2) - 80, self.height / 2 - (ask_position[3] / 2)))
-            self.screen.blit(ask, (self.width /2 -  (ask_position[2]/2) - 80, self.height/2 - (ask_position[3]/2) - 45))
-            #text2 = font.render(password, 1, (100, 100, 100))
-            #screen.blit(text2,(100, 100))
-            #print(self.Player1)
+            self.screen.blit(ask, (self.width /2 -  (ask_position[2]/2), self.height/2 - (ask_position[3]/2) - 45))
+            if username_1_position[2] <= (x - 15):
+                self.screen.blit(username_1, (self.width / 2 - (username_1_position[2] / 2), self.height / 2 - (ask_position[3] / 2)))
+            else:
+                self.name1 = ""
+                self.screen.blit(username_1,
+                                 (self.width / 2 - (username_1_position[2] / 2), self.height / 2 - (ask_position[3] / 2)))
             pygame.display.flip()
+
     def get_name(self):
         return self.Player1
 
@@ -87,78 +65,54 @@ class PlayerName2():
         self.width = 800
         self.height = 575
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.name1 = ""
-        self.Player1 = ""
         self.name2 = ""
         self.Player2 = ""
+        self.distance_border = 20
+        self.draw_frame()
+        self.name(400, 50)
 
-        self.name(400, 50, 0)
-
-    def name(self, x, y, count):
-
-        #password = ""
+    def draw_frame(self):
+        pygame.draw.rect(self.screen, (0, 0, 255),
+                         pygame.Rect((self.distance_border, self.distance_border),
+                                     ((self.width - self.distance_border * 2),
+                                      (self.height - self.distance_border * 2))))
+    def name(self, x, y):
         font = pygame.font.SysFont("arial", 30)
-
         while not False:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if pygame.key.get_pressed():
                         if event.key == pygame.K_BACKSPACE:
-                            if count == 0:
-                                self.name2 = self.name2[:-1]
-                            else:
-                                self.name2 = self.name2[:-1]
+                            self.name2 = self.name2[:-1]
                         elif event.key == pygame.K_ESCAPE:
-                            if count == 0:
-                                self.name2 = ""
-                            else:
-                                self.name2 = ""
+                            self.name2 = ""
                         elif event.key == pygame.K_SPACE:
-                            if count == 0:
-                                self.name2 = self.name2
-                            else:
-                                self.name2 = self.name2
+                            self.name2 = self.name2
                         elif event.key == pygame.K_RETURN:
                             self.Player2 = self.name2
-                            self.Player2 = self.name2
-
                             return True
-
-
-                        #elif event.key == pygame.K_RETURN:
-                        #    if event.key == pygame.K_BACKSPACE:
-                        #        password = password[:-1]
-                        #    elif event.key == pygame.K_SPACE:
-                        #        password = password
-                        #    else:
-                        #        password = password + event.unicode
                         else:
-                            if count == 0:
-                                self.name2 = self.name2 + event.unicode
-                            else:
-                                self.name2 = self.name2 + event.unicode
-
+                            self.name2 = self.name2 + event.unicode
                 elif event.type == pygame.QUIT:
-                    return True
+                    sys.exit()
 
-
-            self.screen.fill((212, 212, 212))
-            text = font.render(self.name2, 1, (255, 255, 255))
-            text2 = font.render(self.name2, 1, (255,255,255))
-
-
-
-
-            ask = font.render("Enter your username", 1 , (255,255,255))
+            username_2 = font.render(self.name2, 1, (255, 255, 255))
+            username_2_positon = username_2.get_rect()
+            print(username_2_positon)
+            ask = font.render("Player 2, enter your username", 1, (255, 255, 255))
             ask_position = ask.get_rect()
-            pygame.draw.rect(self.screen, (100, 100, 100), pygame.Rect(self.width/2 - (x/2), self.height/2 - (y/2), 400, 50))
-            self.screen.blit(text, (self.width /2 -  (ask_position[2]/2) - 80, self.height/2 - (ask_position[3]/2)))
-            self.screen.blit(text2,
-                             (self.width / 2 - (ask_position[2] / 2) - 80, self.height / 2 - (ask_position[3] / 2)))
-            self.screen.blit(ask, (self.width /2 -  (ask_position[2]/2) - 80, self.height/2 - (ask_position[3]/2) - 45))
-            #text2 = font.render(password, 1, (100, 100, 100))
-            #screen.blit(text2,(100, 100))
-            #print(self.Player1)
+            pygame.draw.rect(self.screen, (100, 100, 100),
+                             pygame.Rect(self.width / 2 - (x / 2), self.height / 2 - (y / 2), 400, 50))
+            self.screen.blit(ask,
+                             (self.width / 2 - (ask_position[2] / 2), self.height / 2 - (ask_position[3] / 2) - 45))
+            if username_2_positon[2] <= (x - 15):
+                self.screen.blit(username_2,
+                                 (self.width / 2 - (username_2_positon[2] / 2), self.height / 2 - (ask_position[3] / 2)))
+            else:
+                self.name1 = ""
+                self.screen.blit(username_2,
+                                 (self.width / 2 - (username_2_positon[2] / 2), self.height / 2 - (ask_position[3] / 2)))
             pygame.display.flip()
+
     def get_name(self):
         return self.Player2
