570d87e6a19222a2d43ff62364117d8201dee073
Huip van den Ende <0906768@hr.nl>
2017-01-30 22:54:08 +0100

    add save/load module

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,2 @@
 #the __all__ variable should contain all files in the folder 'modules'. 
-__all__ = ['player', 'grid', 'ships', "mine", 'menu', 'cards', 'login', 'mysql', "highscores", 'win']
+__all__ = ['player', 'grid', 'ships', "mine", 'menu', 'cards', 'login', 'mysql', "highscores", 'win', 'saveload']
