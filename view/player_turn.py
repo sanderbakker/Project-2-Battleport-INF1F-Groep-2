b6c8c7202c7183499b13e6c259f939941fc8c976
Lennart Voogd <lennartvoogd@live.nl>
2017-02-01 15:40:05 +0100

    removed ‘score’ from player turn

--- view/player_turn.py
+++ view/player_turn.py
@@ -1,13 +1,11 @@
 class Show:
 	def __init__(self, Game, Player):	
 		Game.reset_font()
-		Game.draw_text("Turn: " , (100,10))
+		Game.draw_text("Turn: " , (150,10))
 		if(Player.get_color() == 'red'):
 			Game.set_font("inherit", (255, 0, 0))
 		else:
 			Game.set_font("inherit", (0, 0, 255))
 
-		Game.draw_text(str(Player.get_name()), (150, 10))
+		Game.draw_text(str(Player.get_name()), (200, 10))
 		Game.reset_font()
-		Game.draw_text("Score:" + str(Player.get_score()), (300, 10))
-		Game.reset_font()	
