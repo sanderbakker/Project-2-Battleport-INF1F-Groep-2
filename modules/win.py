f6716fd058bc2416d9995b0e3a1ed87d99670b33
Sander Bakker <sanderbakker1999@gmail.com>
2017-02-01 10:03:57 +0100

    Added comments to win module

--- modules/win.py
+++ modules/win.py
@@ -23,7 +23,7 @@ class win_screen:
 		x_position = self.width / 2 - math.floor(entered_text_position[2] / 2)
 		y_position = math.ceil(7 / 8 * (self.height - self.distance_border * 2)) + 4
 		self.screen.blit(entered_text, (x_position, y_position))
-
+	#check if the "red x" is pressed or the exit button is clicked
 	def process_events(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -35,12 +35,13 @@ class win_screen:
 											7 / 8 * (self.height - self.distance_border * 2)) + 35:
 						sys.exit()
 		return False
-
+	# draws frame
 	def draw_frame(self):
 		pygame.draw.rect(self.screen, (212, 212, 212),
 						 pygame.Rect((self.distance_border, self.distance_border),
 									 ((self.width - self.distance_border * 2),
 									  (self.height - self.distance_border * 2))))
+	# displays the win screen
 	def show(self):
 		self.add_or_update_highscores()
 		while not self.process_events():
@@ -50,7 +51,7 @@ class win_screen:
 			self.add_return()
 			self.add_button_text("Exit")
 			pygame.display.flip()
-
+	# draws the name of the winner
 	def draw_winner(self):
 		font_type = pygame.font.SysFont('Arial', 50)
 		text = font_type.render(self.Winner.get_name() + ' WON!', 0, (0,0,0))
@@ -58,9 +59,10 @@ class win_screen:
 		start_width = (self.width - text_width ) / 2
 		start_height = (self.height - text_height) / 8
 		self.screen.blit(text, (start_width, start_height))
+	# gets the name of the winner
 	def get_name(self):
 		return self.Winner
-
+	# get the current highscores
 	def get_highscores(self):
 		mysql_con = mysql.mysql()
 		result = mysql_con.select("SELECT * FROM players ORDER BY score DESC LIMIT 10")
@@ -75,13 +77,14 @@ class win_screen:
 			self.screen.blit(score_result, ((self.width / 2 + 65), x))
 			x = x + 25
 		return player
+	# get the current score of a player
 	def get_score(self):
 		mysql_con = mysql.mysql()
 		name = self.Winner.get_name()
 		score = mysql_con.select('SELECT score FROM players WHERE player_name = "' + str(name) + '"')
 		new_score = score[0].get("score") + 1
 		return new_score
-
+	# gets all the players in the database
 	def get_players(self):
 		mysql_con = mysql.mysql()
 		player_name = mysql_con.select(("SELECT player_name FROM players"))
@@ -89,14 +92,14 @@ class win_screen:
 		for i in range(len(player_name)):
 			name_list.append(player_name[i].get("player_name"))
 		return name_list
-
+	# updates the highscores
 	def update_highscores(self):
 		score = int(self.get_score())
 		name = self.Winner.get_name()
 		mysql_con = mysql.mysql()
 		result = mysql_con.update('UPDATE players SET score= "' + str(score) + '" WHERE player_name = "' + str(name) + '"')
 		#('UPDATE players SET score= "' + str(score) + '" WHERE player_name = "' + str(name) + '"')
-
+	# add a new entry or updates the highscore
 	def add_or_update_highscores(self):
 		mysql_con = mysql.mysql()
 		#check_player = mysql_con.select("SELECT player_name FROM players LIMIT 10")
@@ -109,7 +112,7 @@ class win_screen:
 
 
 
-
+	# adds a return button
 	def add_return(self):
 		pygame.draw.rect(self.screen, (48, 148, 51), pygame.Rect(
 			(self.width / 2 - 50, math.ceil(7 / 8 * (self.height - self.distance_border * 2))), (100, 35)))
